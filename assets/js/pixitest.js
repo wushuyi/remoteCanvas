5d56e7ceca2bc7476e18ee8102df2e9421fd0e19
巫书轶 <wushuyi541@gmail.com>
2014-10-29 14:04:14 +0800

    基本 功能 已经 实现

--- assets/js/pixitest.js
+++ assets/js/pixitest.js
@@ -8,22 +8,24 @@
         renderer.view.style.display = "block";
         var $board = $('#board');
         $board.append(renderer.view);
-        var container = new PIXI.DisplayObjectContainer();
+
         var loader = new PIXI.AssetLoader([
             "./assets/image/bg.png"
         ]);
         loader.on('onComplete', function(){
+            var bgcontainer = new PIXI.DisplayObjectContainer();
+            stage.addChildAt(bgcontainer, 0);
+
             var texture = new PIXI.Texture.fromImage('./assets/image/bg.png');
             var bg = new PIXI.Sprite(texture);
-            stage.addChildAt(bg, 0);
-            stage.addChildAt(bg, 0);
 
-            window.texture1 = texture;
+            bgcontainer.addChild(bg);
+
             renderer.render(stage);
         });
         loader.load();
 
-        function pan(){
+        function pen(){
             var boardControl = {
                 oldP : {
 
@@ -31,86 +33,92 @@
                 isDown: false
             };
             $board.off(".board");
+            var pencontainer;
             var graphics;
-            var glist = [];
-            var leth = 0;
-            $board.on('mousedown.pan.board touchstart.pan.board', function(e){
+            var graphicsStep = 0;
+            var graphicsDatastep = 0;
+
+            function drawPan(P){
+               //console.log(P);
+                console.log("graphicsDatastepL " + graphicsDatastep);
+                if(!pencontainer){
+                    //console.log('run');
+                    pencontainer = new PIXI.DisplayObjectContainer();
+                    stage.addChildAt(pencontainer, stage.children.length);
+
+                    graphics = new PIXI.Graphics();
+                    graphics.lineStyle(2, 0xFF0000, 1);
+                    pencontainer.addChild(graphics);
+                }else{
+                    if(graphicsStep == 1000){
+                        pencontainersds = new PIXI.DisplayObjectContainer();
+                        stage.addChildAt(pencontainer, stage.children.length);
+                        graphicsStep == 0;
+                    }
+                    if(graphicsDatastep == 1000){
+                        graphics = new PIXI.Graphics();
+                        graphics.lineStyle(2, 0xFF0000, 1);
+                        pencontainer.addChild(graphics);
+                        graphicsDatastep = 0;
+                        graphicsStep += 1;
+                    }
+                }
+
+                switch (P.event){
+                    case "down":
+
+                        graphics.moveTo(P.offsetX, P.offsetY);
+                        break;
+                    case "move":
+                        graphics.lineTo(P.offsetX , P.offsetY);
+                        renderer.render(stage);
+                        graphics.moveTo(boardControl.oldP.offsetX, boardControl.oldP.offsetY);
+                        graphicsDatastep += 1;
+                        break;
+                    case "up":
+
+                        break;
+                }
+            }
+
+            $board.on('mousedown.pen.board touchstart.pen.board', function(e){
                 boardControl.isDown = true;
                 var P = {
-                    type: "down",
+                    type: "pan",
+                    event: "down",
                     offsetX : e.offsetX,
                     offsetY : e.offsetY
                 };
-
+                drawPan(P);
                 boardControl.oldP = P;
-                leth += 1;
-                glist[leth] = new PIXI.Graphics();
-                stage.addChildAt(glist[leth], stage.children.length);
-                graphics = glist[leth];
-                window.graphics = graphics;
-                graphics.lineStyle(2, 0xFF0000, 1);
-
-                graphics.moveTo(P.offsetX, P.offsetY);
-                /*
-                 graphics = new PIXI.Graphics();
-                 stage.addChildAt(graphics, 0);
-                 graphics.lineStyle(4, 0xFF0000, 1);
-                 */
             });
-            $board.on('mousemove.pan.board touchmove.pan.board', function(e){
+            $board.on('mousemove.pen.board touchmove.pen.board', function(e){
                 if(!boardControl.isDown){
                     return false;
                 }
                 var P = {
-                    type: "move",
+                    type: "pan",
+                    event: "move",
                     offsetX : e.offsetX,
                     offsetY : e.offsetY
                 };
-
-                //graphics.beginFill(0xFF0000, 1);
-                //graphics.drawCircle(P.offsetX , P.offsetY, 4, 4, 2);
-                //graphics.endFill();
-                //renderer.render(stage);
-
-
-                //var calX = P.offsetX - boardControl.oldP.offsetX;
-                //var calY = P.offsetY - boardControl.oldP.offsetY;
-                //var result = Math.pow((calX *calX + calY * calY), 0.5);
-                //if(result > 2){
-                //    //graphics.lineStyle(1, 0xFF0000, 1);
-                //    graphics.beginFill(0xFF0000, 1);
-                //    graphics.drawRect(P.offsetX , P.offsetY, 4, 4);
-                //    graphics.endFill();
-                //}else{
-                //
-                //}
-
-                graphics.lineTo(P.offsetX , P.offsetY);
-
-
-                //console.log(result);
-                renderer.render(stage);
-
-                //graphics.lineTo(P.offsetX , P.offsetY);
-                //renderer.render(stage);
-                graphics.moveTo(boardControl.oldP.offsetX, boardControl.oldP.offsetY);
-                //console.log(graphics.graphicsData.length);
+                drawPan(P);
                 boardControl.oldP = P;
             });
-            $board.on('mouseup.pan.board mouseout.pan.board touchend.pan.board touchcancel.pan.board', function(e){
+            $board.on('mouseup.pen.board mouseout.pen.board touchend.pen.board touchcancel.pen.board', function(e){
                 boardControl.isDown = false;
                 var P = {
-                    type: "up",
+                    type: "pan",
+                    event: "up",
                     offsetX : e.offsetX,
                     offsetY : e.offsetY
                 };
+                drawPan(P);
                 boardControl.oldP = {};
-                //render(ctx[0], P);
-                //console.log(e);
             });
         }
-        //pan();
-        window.pan = pan;
+        //pen();
+        window.pen = pen;
 
         function eraser() {
             var baseTexture = new PIXI.BaseTexture.fromImage('./assets/image/bg.png');
@@ -119,40 +127,74 @@
                 isDown: false
             };
             $board.off(".board");
+
+
+
+            var erasercontainer;
+            var erasercontainerStep = 0;
+
+            function drawEraser(P){
+                if(!erasercontainer){
+                    erasercontainer = new PIXI.DisplayObjectContainer();
+                    stage.addChildAt(erasercontainer, stage.children.length);
+                }else{
+                    if(erasercontainerStep == 1000){
+                        erasercontainer = new PIXI.DisplayObjectContainer();
+                        stage.addChildAt(erasercontainer, stage.children.length);
+                    }
+                }
+                switch (P.event){
+                    case "down":
+                        break;
+                    case "move":
+                        var texture = new PIXI.Texture(baseTexture, new PIXI.Rectangle(P.offsetX , P.offsetY, 30, 30));
+                        var eraser = new PIXI.Sprite(texture);
+                        eraser.position.x = P.offsetX;
+                        eraser.position.y = P.offsetY;
+                        erasercontainer.addChild(eraser);
+                        erasercontainerStep += 1;
+                        renderer.render(stage);
+                        break;
+                    case "up":
+                        break;
+                }
+            }
+
+
             $board.on('mousedown.eraser.board touchstart.eraser.board', function(e){
                 boardControl.isDown = true;
                 var P = {
-                    type: "down",
+                    type: "eraser",
+                    event: "down",
                     offsetX : e.offsetX,
                     offsetY : e.offsetY
                 };
+
+                drawEraser(P);
+
+                boardControl.oldP = P;
             });
             $board.on('mousemove.eraser.board touchmove.eraser.board', function(e){
                 if(!boardControl.isDown){
                     return false;
                 }
                 var P = {
-                    type: "move",
+                    type: "eraser",
+                    event: "move",
                     offsetX : e.offsetX,
                     offsetY : e.offsetY
                 };
-
-                var texture2 = new PIXI.Texture(baseTexture, new PIXI.Rectangle(P.offsetX , P.offsetY, 30, 30));
-
-                window.texture2 = texture2;
-                var eraser = new PIXI.Sprite(texture2);
-                eraser.position.x = P.offsetX;
-                eraser.position.y = P.offsetY;
-                stage.addChildAt(eraser, stage.children.length);
-                renderer.render(stage);
+                drawEraser(P);
             });
             $board.on('mouseup.eraser.board mouseout.eraser.board touchend.eraser.board touchcancel.eraser.board', function(e){
                 boardControl.isDown = false;
                 var P = {
-                    type: "up",
+                    type: "eraser",
+                    event: "up",
                     offsetX : e.offsetX,
                     offsetY : e.offsetY
                 };
+                boardControl.oldP = P;
             });
         }
         window.eraser = eraser;
